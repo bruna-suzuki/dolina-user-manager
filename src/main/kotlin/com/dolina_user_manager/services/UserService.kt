49bfc1af6a64fea3530279248a9df73ebb80a39a
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-11 17:38:50 -0300

    feat: add createUser function

---
+++ src/main/kotlin/com/dolina_user_manager/services/UserService.kt
@@ -0,0 +1,21 @@
+package com.dolina_user_manager.services
+
+import com.dolina_user_manager.dtos.UserDto
+import com.dolina_user_manager.mappers.toUserModel
+import com.dolina_user_manager.models.UserModel
+import com.dolina_user_manager.producers.UserProducer
+import com.dolina_user_manager.repositories.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+    private val userProducer: UserProducer) {
+
+    fun createUser(payload: UserDto): UserModel {
+        val newUser = payload.toUserModel()
+        userProducer.publishMessage(newUser)
+        userRepository.save(newUser)
+        return newUser
+    }
+}

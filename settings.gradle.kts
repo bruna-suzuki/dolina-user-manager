1617c5f8ebf561d1316a2e751481c7cbb3d6a00a
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-11 17:22:43 -0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "dolina-user-manager"
---
+++ src/main/kotlin/com/dolina_user_manager/DolinaUserManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.dolina_user_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DolinaUserManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DolinaUserManagerApplication>(*args)
+}

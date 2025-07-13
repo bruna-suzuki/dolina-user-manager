1d5b01c9796c881f5dd15f8c1de98a9aee4bcc0d
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-13 14:41:09 -0300

    feat: add deletion producer

--- src/main/kotlin/com/dolina_user_manager/producers/UserProducer.kt
+++ src/main/kotlin/com/dolina_user_manager/producers/UserProducer.kt
@@ -1,5 +1,6 @@
 package com.dolina_user_manager.producers
 
+import com.dolina_user_manager.dtos.MessageDeletionDto
 import com.dolina_user_manager.dtos.MessageWelcomeDto
 import com.dolina_user_manager.models.UserModel
 import org.springframework.amqp.rabbit.core.RabbitTemplate
@@ -12,11 +13,22 @@ class UserProducer(private val rabbitTemplate: RabbitTemplate) {
     @Value("\${broker.queue.email.name}")
     lateinit var queueName: String
 
+    @Value("\${broker.queue.delete.email.name}")
+    lateinit var deleteUserQueue: String
+
     fun publishMessage(userModel: UserModel) {
         val message = MessageWelcomeDto(
             fullName = userModel.fullName,
             email = userModel.email
         )
         rabbitTemplate.convertAndSend("", queueName, message)
+    }
+
+    fun publishDeleteMessage(userModel: UserModel) {
+        val message = MessageDeletionDto(
+            userName = userModel.fullName,
+            userEmail = userModel.email
+        )
+        rabbitTemplate.convertAndSend("", deleteUserQueue, message)
     }
 }

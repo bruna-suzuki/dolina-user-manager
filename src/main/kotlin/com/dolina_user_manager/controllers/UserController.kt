22b913166a312169e51e791b73ee49aea9557d9c
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-13 14:40:08 -0300

    feat: add delete user function

--- src/main/kotlin/com/dolina_user_manager/controllers/UserController.kt
+++ src/main/kotlin/com/dolina_user_manager/controllers/UserController.kt
@@ -5,10 +5,13 @@ import com.dolina_user_manager.models.UserModel
 import com.dolina_user_manager.services.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/user")
@@ -18,5 +21,11 @@ class UserController(private val userService: UserService) {
     fun createUser(@RequestBody payload: UserDto): ResponseEntity<UserModel> {
         val newUser = userService.createUser(payload)
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser)
+    }
+
+    @DeleteMapping("/delete/{id}")
+    fun deleteUser(@PathVariable id: UUID): ResponseEntity<String> {
+        userService.deleteUser(id)
+        return ResponseEntity.status(HttpStatus.OK).body("User has been deleted.")
     }
 }

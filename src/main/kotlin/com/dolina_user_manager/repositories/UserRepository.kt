83473f5ab8472d44d8448d408b0a2ed0086f89b3
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-24 11:05:11 -0300

    feat: add global user id

--- src/main/kotlin/com/dolina_user_manager/repositories/UserRepository.kt
+++ src/main/kotlin/com/dolina_user_manager/repositories/UserRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface UserRepository : JpaRepository<UserModel, UUID>{
+    fun findByGlobalUserId(globalUserId: UUID): UserModel?
 }

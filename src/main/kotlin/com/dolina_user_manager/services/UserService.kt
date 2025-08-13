f0b6f724c925effca906bed63d813f6201c81446
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-08-13 14:13:55 -0300

    feat: add validate purchase

--- src/main/kotlin/com/dolina_user_manager/services/UserService.kt
+++ src/main/kotlin/com/dolina_user_manager/services/UserService.kt
@@ -1,6 +1,9 @@
 package com.dolina_user_manager.services
 
+import com.dolina_user_manager.dtos.AddMoneyDto
 import com.dolina_user_manager.dtos.UserDto
+import com.dolina_user_manager.dtos.requests.RequestUserValidatorDto
+import com.dolina_user_manager.dtos.responses.ResponseUserValidatorDto
 import com.dolina_user_manager.models.UserModel
 import com.dolina_user_manager.producers.UserProducer
 import com.dolina_user_manager.repositories.UserRepository
@@ -38,6 +41,20 @@ class UserService(
         return userRepository.findAll()
     }
 
+    fun validatePurchase(payload: RequestUserValidatorDto): ResponseUserValidatorDto {
+        val user = findByGlobalId(payload.globalUserId) ?: throw Exception("User not found.")
+        val balance = validateBalance(payload.amount, payload.globalUserId)
+        if (balance) {
+            val response = ResponseUserValidatorDto(
+                userName = user.fullName,
+                address = user.address,
+                email = user.email)
+            return response
+        } else {
+            throw Exception("Insufficient balance.")
+        }
+    }
+
     fun findByGlobalId(globalUserId: UUID): UserModel? {
         return userRepository.findByGlobalUserId(globalUserId)
     }
@@ -49,5 +66,20 @@ class UserService(
         } else {
             return false
         }
+    }
+
+    fun updateBalance(globalUserId: UUID, amount: BigDecimal) {
+        val user = findByGlobalId(globalUserId) ?: throw Exception("User not found.")
+
+        user.balance = user.balance.subtract(amount)
+        userRepository.save(user)
+    }
+
+    fun addMoney(payload: AddMoneyDto) {
+        val user = findByGlobalId(payload.globalUserId)
+            ?: throw Exception("User not found.")
+
+        user.balance = user.balance.add(payload.amount)
+        userRepository.save(user)
     }
 }
